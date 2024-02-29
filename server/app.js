7dad410e853d6e5e8c25011df645ade8ff041bbe
beetleOf <beet1le@mail.ru>
2024-02-29 15:49:13 +0300

    commentRouter

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const tourRouter = require('./routes/tourRouter');
 const equipRouter = require('./routes/equipRouter')
 const tokensRouter = require('./routes/tokensRouter');
 const userRouter = require('./routes/userRouter');
+const commentRouter = require('./routes/commentRouter')
 
 app.use(cors({ credentials: true, origin: true }));
 app.use(morgan('dev'));
@@ -24,4 +25,6 @@ app.use('/api/tour', tourRouter);
 app.use('/api/equip', equipRouter)
 app.use('/api/auth', userRouter);
 app.use('/api/tokens', tokensRouter);
+app.use('/api/comments', commentRouter);
+app.use('/api/favourite')
 module.exports = app;
