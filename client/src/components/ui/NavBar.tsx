64e44f834896ffbb0f0746d596712acf6776a58b
RuslanKhatipov <rmk.educ@gmail.com>
2024-02-29 15:40:43 +0300

    mm

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, HStack, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, HStack, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHook';
@@ -11,26 +11,49 @@ export default function NavBar(): JSX.Element {
     void dispatch(logOutThunk());
   };
   return (
-    <Box bg={useColorModeValue('gray.200', 'gray.900')} rounded="lg" px={4}>
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        <HStack spacing={6}>
+    <Box bg="transparent" px={4} fontSize={20}>
+      <Flex h={24} alignItems="center" justifyContent="space-between">
+        <HStack spacing={6} color="black">
           <Box>Hi, {user.status === 'logged' ? user.username : 'guest'}</Box>
           <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
-            groups
+            Туры
+          </NavLink>
+          <NavLink to="/rent" className={({ isActive }) => (isActive ? 'active' : '')}>
+            {' '}
+            {/* Исправлено на /rent для Аренды */}
+            Аренда
           </NavLink>
         </HStack>
-        <HStack spacing={6}>
-          {user.status !== 'logged' ? (
+        <HStack>
+        <Avatar
+                as={NavLink}
+                to={`/user/${user.id}`} 
+                size="lg"
+                src="../../../public/logo.svg"
+              />
+              <Box fontSize={30}>GOODTRIP</Box>
+        </HStack>
+        <HStack spacing={6} color="black">
+          {user && user.status !== 'logged' ? ( // Исправлена проверка наличия пользователя
             <>
-              <NavLink to="signin" className={({ isActive }) => (isActive ? 'active' : '')}>
-                signin
+            
+              <NavLink to="/signin" className={({ isActive }) => (isActive ? 'active' : '')}>
+                Войти
               </NavLink>
-              <NavLink to="signup" className={({ isActive }) => (isActive ? 'active' : '')}>
-                signup
+              <NavLink to="/signup" className={({ isActive }) => (isActive ? 'active' : '')}>
+                Зарегистрироваться
               </NavLink>
             </>
           ) : (
-            <Button onClick={logoutHandler}>Выйти</Button>
+            <>
+              <Avatar
+                as={NavLink}
+                to={`/user/${user.id}`} 
+                size="sm"
+                src="https://www.svgrepo.com/download/6595/beer.svg"
+              />
+              <Button onClick={logoutHandler}>Выйти</Button>
+            </>
           )}
         </HStack>
       </Flex>
--- server/routes/itemRouter.js
+++ server/routes/itemRouter.js
@@ -1,25 +1,25 @@
-const itemRouter = require("express").Router();
-const fs = require("fs").promises;
-const sharp = require("sharp");
+const itemRouter = require('express').Router();
+const fs = require('fs').promises;
+const sharp = require('sharp');
 
-const { Item, Category } = require("../db/models");
-const upload = require("../middlewares/multerMid");
+const { Item, Category } = require('../db/models');
+const upload = require('../middlewares/multerMid');
 
-itemRouter.get("/", async (req, res) => {
+itemRouter.get('/', async (req, res) => {
   const items = await Category.findAll({
     include: Item,
   });
   res.json(items);
 });
 
-itemRouter.post("/", upload.single("file"), async (req, res) => {
+itemRouter.post('/', upload.single('file'), async (req, res) => {
   const { name, description, price, cat_id } = req.body;
   console.log(req.body);
   if (!name || !description || !price || !cat_id) {
-    return res.status(400).json({ error: "All fields are required" });
+    return res.status(400).json({ error: 'All fields are required' });
   }
   if (!req.file) {
-    return res.status(400).json({ message: "File not found" });
+    return res.status(400).json({ message: 'File not found' });
   }
 
   const fileName = `${Date.now()}.webp`;
@@ -35,18 +35,18 @@ itemRouter.post("/", upload.single("file"), async (req, res) => {
   res.json(newItem);
 });
 
-itemRouter.delete("/:id", async (req, res) => {
+itemRouter.delete('/:id', async (req, res) => {
   const { id } = req.params;
   if (Number.isNaN(+id) || Number(id) <= 0) {
-    return res.status(400).json({ error: "Id is invalid" });
+    return res.status(400).json({ error: 'Id is invalid' });
   }
   const item = await Item.findByPk(id);
   if (!item) {
-    return res.status(404).json({ error: "Item not found" });
+    return res.status(404).json({ error: 'Item not found' });
   }
   await fs.unlink(`./public/img/${item.path}`).catch((e) => console.log(e));
   await item.destroy({ where: { id } });
-  res.json({ message: "Item deleted" });
+  res.json({ message: 'Item deleted' });
 });
 
 module.exports = itemRouter;
