7c37f8472060f1697be864f4e96328d78bf80cf8
beetleOf <beet1le@mail.ru>
2024-02-29 13:26:11 +0300

    endpoints maked

---
+++ server/routes/equipRouter.js
@@ -0,0 +1,52 @@
+const equipRouter = require('express').Router();
+const fs = require('fs').promises;
+const sharp = require('sharp');
+
+const { Equipment, CategoryEquipment } = require('../db/models');
+const upload = require('../middlewares/multerMid');
+
+equipRouter.get('/', async (req, res) => {
+  const equips = await CategoryEquipment.findAll({
+    include: Equipment,
+  });
+  res.json(equips);
+});
+
+equipRouter.post('/', upload.single('file'), async (req, res) => {
+  const { name, description, price, catEId } = req.body;
+  console.log(req.body);
+  if (!name || !description || !price || !catEId) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+  if (!req.file) {
+    return res.status(400).json({ message: 'Equip not found' });
+  }
+
+  const fileName = `${Date.now()}.webp`;
+  const outputBuffer = await sharp(req.file.buffer).webp().toBuffer();
+  await fs.writeFile(`./public/img/${fileName}`, outputBuffer);
+  const newEquip = await Equipment.create({
+    name,
+    description,
+    price,
+    catEId,
+    pathImg: fileName,
+  });
+  return res.json(newEquip);
+});
+
+equipRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (Number.isNaN(+id) || Number(id) <= 0) {
+    return res.status(400).json({ error: 'Id is invalid' });
+  }
+  const equip = await Equipment.findByPk(id);
+  if (!equip) {
+    return res.status(404).json({ error: 'Equipment not found' });
+  }
+  await fs.unlink(`./public/img/${equip.path}`).catch((e) => console.log(e));
+  await Equipment.destroy({ where: { id } });
+  res.json({ message: 'Equipment deleted' });
+});
+
+module.exports = equipRouter;
