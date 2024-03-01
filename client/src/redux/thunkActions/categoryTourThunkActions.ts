148508c35f8f09ca7d46d223fed2be68f953ec8e
beetleOf <beet1le@mail.ru>
2024-03-01 13:39:29 +0300

    catTourThunk

---
+++ client/src/redux/thunkActions/categoryTourThunkActions.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import apiService from '../../services/categoryTourService';
+import type { CategoryTourType } from '../../types/tourType';
+
+export const getAllCategoryTourThunk = createAsyncThunk<CategoryTourType[]>(
+  'categoryTour/all',
+  async () => {
+    const allCategoryTour = await apiService.getCategoryTour();
+    return allCategoryTour;
+  },
+);
+
+export const addCategoryTourThunk = createAsyncThunk<CategoryTourType, { name: string }>(
+  'categoryTour/add',
+  async (obj) => {
+    const categoryTour = await apiService.addCategoryTour(obj);
+    return categoryTour;
+  },
+);
+
+export const deleteCategoryTourThunk = createAsyncThunk<number, number>(
+  'categoryTour/delete',
+  async (id) => {
+    await apiService.deleteCategoryTour(id);
+    return id;
+  },
+);
+
+export const changeCategoryTourThunk = createAsyncThunk<CategoryTourType, number>(
+  'categoryTour/change',
+  async (id) => {
+    const categoryTourUpdated = await apiService.changeCategoryTour(id);
+    return categoryTourUpdated;
+  },
+);
