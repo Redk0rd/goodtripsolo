53354e09608b9559254f75c33f973664742edfe7
Redk0rd <red.kord95@gmail.com>
2024-03-01 15:32:03 +0300

    ewew

--- client/src/redux/slices/rourSlice.ts
+++
@@ -1,5 +0,0 @@
-import type { PayloadAction } from '@reduxjs/toolkit';
-
-import { createSlice } from '@reduxjs/toolkit';
-
-import type {  } from '../../types/tourType'
---
+++ client/src/redux/slices/tourSlice.ts
@@ -0,0 +1,30 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { CategoryTourType } from '../../types/tourType'
+
+
+
+const initialState: CategoryTourType ={
+    id: 0,
+    name: '',
+    tours: []
+}
+
+
+const tourSlice = createSlice({
+    name: 'tour',
+    initialState,
+    reducers: {},
+
+    extraReducers: (builder) => {
+        builder.addCase(getAllCategoryTourThunk.fulfilled, (state, action) => {
+            state = action.payload
+        })
+
+        // builder.addCase(addCategoryTourThunk.)
+    }
+
+
+})
