8c851d5719316d8cc0252ba5b078f30968b492ea
DmitryZhuck <RammDima@yandex.ru>
2024-03-01 18:26:35 +0300

    сделал скелет 2

--- client/src/components/ui/userPage/userPageFeedback/UserPageFeedback.tsx
+++ client/src/components/ui/userPage/userPageFeedback/UserPageFeedback.tsx
@@ -1,13 +1,15 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
-import './userPageFeedback.css'
+import './userPageFeedback.css';
 
 export default function UserPageFeedback(): JSX.Element {
   return (
-    <Box>
-      <Text>Отзывы о пользователе</Text>
-      <Box>
-        <Flex>{/* тут мапать комменты в столбик */}</Flex>
+    <Box className="wrapper">
+      <Box className="container">
+        <Text className='main_title'>Отзывы о пользователе</Text>
+        <Box>
+          <Flex>{/* тут мапать комменты в столбик */}</Flex>
+        </Box>
       </Box>
     </Box>
   );
--- client/src/components/ui/userPage/userPageTours/UserPageTours.tsx
+++ client/src/components/ui/userPage/userPageTours/UserPageTours.tsx
@@ -1,21 +1,25 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
-import './userPageToursStyle.css'
+import './userPageToursStyle.css';
 
 export default function UserPageTours(): JSX.Element {
   return (
-    <Box>
-      <Text>Мои туры:</Text>
+    <Box className="wrapper_tours">
+      <Box>
+        <Box className="container_tours_flex">
+          <Text className='main_title'>Мои туры:</Text>
 
-      <Flex>
-        <Text>участник</Text>
-        <Box>{/* тут мапать карточки гридом ??? */}</Box>
-      </Flex>
+          <Flex className='flex_column'>
+            <Text className='text_description_who'>участник</Text>
+            <Box className='box'>{/* тут мапать карточки гридом ??? */}</Box>
+          </Flex>
 
-      <Flex>
-        <Text>организатор</Text>
-        <Box>{/* тут мапать карточки гридом ??? */}</Box>
-      </Flex>
+          <Flex className='flex_column'>
+            <Text className='text_description_who'>организатор</Text>
+            <Box className='box'>{/* тут мапать карточки гридом ??? */}</Box>
+          </Flex>
+        </Box>
+      </Box>
     </Box>
   );
 }
