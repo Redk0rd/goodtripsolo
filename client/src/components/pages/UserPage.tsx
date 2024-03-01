369f6f944156273ba0054a597814875875fa0d5c
DmitryZhuck <RammDima@yandex.ru>
2024-03-01 15:37:55 +0300

    init userPage

--- client/src/components/pages/UserPage.tsx
+++ client/src/components/pages/UserPage.tsx
@@ -3,11 +3,11 @@ import { Box } from '@chakra-ui/react';
 import UserPageAboutUser from '../ui/userPage/userPageAboutUser/UserPageAboutUser';
 import UserPageFeedback from '../ui/userPage/userPageFeedback/UserPageFeedback';
 import UserPageTours from '../ui/userPage/userPageTours/UserPageTours';
-import '../ui/userPage/userPage.css'
+import '../ui/userPage/userPage.css';
 
 export default function UserPage(): JSX.Element {
   return (
-    <Box>
+    <Box mt={0} p={0}>
       <UserPageAboutUser />
       <UserPageTours />
       <UserPageFeedback />
