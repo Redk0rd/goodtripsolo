b3f3ff170718f534b386883d60ccd8b15941db58
DmitryZhuck <RammDima@yandex.ru>
2024-03-05 12:24:30 +0300

    motion done

--- client/src/components/ui/MainPageUi/AboutUsBot.tsx
+++ client/src/components/ui/MainPageUi/AboutUsBot.tsx
@@ -1,34 +1,47 @@
 import { Box, Text, Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import { motion } from 'framer-motion';
+import './AboutUsStyle.css';
+import { textAnimation, textAnimation2, textAnimationTitle, containerOpen } from './motion';
 
 export default function AboutUsTop(): JSX.Element {
   return (
     <Box className="wrappre_container" bg="#714e3b">
-      <Flex className="container">
-        <Text className="main_title">Преимущества GoodTrip?</Text>
-        <Flex className="wrapper_img_and_text" bg="#714e3b">
-          <Box position="relative" className="img_wrapper">
-            <Image
-              className="img_top"
-              src="https://i.pinimg.com/736x/76/a6/9e/76a69e161a7e2b0ae2986a6632ed42a1.jpg"
-              alt="Dan Abramov"
-            />
-          </Box>
-          <Flex className="text_wrapper_flex" bg="#714e3b">
-            <Text className="title">Что такое GoodTrip?</Text>
-            <Text className="description">
-              GoodTrip — проект о приключениях, свершениях, преодолениях и победах. Мы верим, что
-              путешествия — лучший способ познания мира и самого себя.
-            </Text>
-            <Text className="title">Почему вам втоит выбрать GoodTrip?</Text>
-            <Text className="description">
-              Любите ли вы походы или никогда в них не бывали, желаете ли открывать новые места или
-              уже известные с новых сторон, а может, просто ищете новые впечатления? Вам с нами по
-              пути!
-            </Text>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: 0.2 }}
+        variants={containerOpen}
+        custom={0.8}
+      >
+        <Flex className="container">
+          <Text as={motion.h1} className="main_title" custom={1} variants={textAnimationTitle}>
+            Преимущества GoodTrip?
+          </Text>
+          <Flex className="wrapper_img_and_text" bg="#714e3b">
+            <Box position="relative" className="img_wrapper">
+              <Image
+                className="img_top"
+                src="https://i.pinimg.com/736x/76/a6/9e/76a69e161a7e2b0ae2986a6632ed42a1.jpg"
+                alt="Dan Abramov"
+              />
+            </Box>
+            <Flex className="text_wrapper_flex" bg="#714e3b">
+              <Text as={motion.h1} className="title" custom={0.8} variants={textAnimation}>Что такое GoodTrip?</Text>
+              <Text className="description" as={motion.h1} custom={1.5} variants={textAnimation2}>
+                GoodTrip — проект о приключениях, свершениях, преодолениях и победах. Мы верим, что
+                путешествия — лучший способ познания мира и самого себя.
+              </Text>
+              <Text as={motion.h1} className="title" custom={0.8} variants={textAnimation}>Почему вам втоит выбрать GoodTrip?</Text>
+              <Text className="description" as={motion.h1} custom={2} variants={textAnimation2}>
+                Любите ли вы походы или никогда в них не бывали, желаете ли открывать новые места
+                или уже известные с новых сторон, а может, просто ищете новые впечатления? Вам с
+                нами по пути!
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      </motion.div>
     </Box>
   );
 }
--- client/src/components/ui/MainPageUi/AboutUsMid.tsx
+++ client/src/components/ui/MainPageUi/AboutUsMid.tsx
@@ -1,53 +1,54 @@
-import { Text, Flex, Image, Box } from '@chakra-ui/react';
+import { Box, Text, Flex, Image, Button } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import React from 'react';
+import './AboutUsStyle.css';
+import { textAnimation, textAnimation2, textAnimationTitle, containerOpen } from './motion';
 
 export default function AboutUs(): JSX.Element {
   return (
     <Box className="wrappre_container" bg="#908059db">
-      <Flex className="container">
-        <Text className="main_title">Что может предложить GoodTrip?</Text>
-        <Flex className="wrapper_img_and_text" direction="row-reverse">
-          <Box position="relative" className="img_wrapper">
-            <Image
-              className="img_top"
-              src="https://turizm.pibig.info/uploads/posts/2023-10/1697110938_turizm-pibig-info-p-neobkhodimii-nabor-v-pokhod-instagram-64.jpg"
-              alt="Dan Abramov"
-            />
-          </Box>
-          <Flex className="text_wrapper_flex">
-              <Text className="title">Что GoodTrip предлагает?</Text>
-              <Text className="description">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: 0.2 }}
+        variants={containerOpen}
+        custom={0.8}
+      >
+        <Flex className="container">
+          <Text as={motion.h1} className="main_title" custom={1} variants={textAnimationTitle}>
+            Что может предложить GoodTrip?
+          </Text>
+          <Flex className="wrapper_img_and_text" direction="row-reverse">
+            <Box position="relative" className="img_wrapper">
+              <Image
+                className="img_top"
+                src="https://turizm.pibig.info/uploads/posts/2023-10/1697110938_turizm-pibig-info-p-neobkhodimii-nabor-v-pokhod-instagram-64.jpg"
+                alt="Dan Abramov"
+              />
+            </Box>
+            <Flex className="text_wrapper_flex">
+              <Text as={motion.h1} className="title" custom={0.8} variants={textAnimation}>
+                Что GoodTrip предлагает?
+              </Text>
+              <Text className="description" as={motion.h1} custom={1.5} variants={textAnimation2}>
                 Мы организуем походы и экспедиции в уникальные и труднодоступные места нашей
                 планеты, выбираем самые красивые и интересные локации и не делим мир на государства.
                 Наши путешествиям могут проходить, где угодно: за полярным кругом, в сердце Африки,
                 на одном из островов в Океании. Для нас нет границ!
               </Text>
-              <Text className="title">Что GoodTrip предоставляет?</Text>
-              <Text className="description">
+              <Text as={motion.h1} className="title" custom={1.6} variants={textAnimation}>
+                Что GoodTrip предоставляет?
+              </Text>
+              <Text className="description" as={motion.h1} custom={2} variants={textAnimation2}>
                 Мы предоставляем все необходимое групповое снаряжение для конкретного путешествия,
                 которое проверено в суровых походных условиях и зарекомендовало себя с лучшей
                 стороны. Необходимая личная экипировка предоставляется в аренду по очень
                 привлекательной цене.
               </Text>
-            {/* <UnorderedList>
-            <Flex direction="column">
-              <ListItem as="kbd" fontSize="1xl" color="white">
-                Lorem ipsum dolor sit amet
-              </ListItem>
-              <ListItem as="kbd" fontSize="1xl" color="white">
-                Consectetur adipiscing elit
-              </ListItem>
-              <ListItem as="kbd" fontSize="1xl" color="white">
-                Integer molestie lorem at massa
-              </ListItem>
-              <ListItem as="kbd" fontSize="1xl" color="white">
-                Facilisis in pretium nisl aliquet
-              </ListItem>
             </Flex>
-          </UnorderedList> */}
           </Flex>
         </Flex>
-      </Flex>
+      </motion.div>
     </Box>
   );
 }
